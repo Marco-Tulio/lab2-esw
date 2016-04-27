0dc268f9fd44e719892fcf9fa821362629e2757f
Marco-Tulio <tulio.rodrigues@gmail.com>
2016-04-27 08:25:27 -0300

    Signed-off-by: Marco-Tulio <tulio.rodrigues@gmail.com>

---
+++ UIT-LAB2/src/br/edu/uit/lab2esw/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.edu.uit.lab2esw;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Classe");
+
+	}
+
+}
